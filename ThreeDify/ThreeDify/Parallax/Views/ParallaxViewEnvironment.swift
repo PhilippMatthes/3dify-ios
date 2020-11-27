0d00f2bfda70e279dc0a89dca3c08891f5e0fcb6
PhilippMatthes <github@philippmatth.es>
2020-11-27 10:24:21 +0100

    Add copyright header to files

--- ThreeDify/ThreeDify/Parallax/Views/ParallaxViewEnvironment.swift
+++ ThreeDify/ThreeDify/Parallax/Views/ParallaxViewEnvironment.swift
@@ -1,3 +1,15 @@
+//
+// 3Dify App
+//
+// Project website: https://github.com/3dify-app
+//
+// Authors:
+// - Philipp Matthes 2020, Contact: mail@philippmatth.es
+//
+// Copyright notice: All rights reserved by the authors given above. Do not
+// remove or change this copyright notice without confirmation of the authors.
+//
+
 import Foundation
 
 class ParallaxViewEnvironment: NSObject, ObservableObject {
